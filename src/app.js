c7c79e6882fd5957aadce7aa764206e950c69b17
ButterKing5000 <butterking5000@gmail.com>
2019-03-31 21:03:33 -0600

    Completed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,138 @@
-class App extends React.Component {
-  render() {
-    return (
-      <h1 className="title">GMod Addon Template Generator</h1>
+const {remote, ipcRenderer} = require("electron");
+const React = require("react");
+const ReactDOM = require("react-dom");
+const win = remote.getCurrentWindow()
+const size = win.getSize();
 
-      // <div
+const messageColor = "#0080ff";
+const inputColor = "#FFF";
+
+let app;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lines: [],
+    }
+    this.responses = [];
+    this.responseData = {};
+
+    this.addResponse("name", "What should the name of your addon folder be?")
+    this.addResponse("file_key", "What will file key be? (eg: fileKey_sv.lua)")
+    this.addResponse("global", "What will your global table be?")
+    this.addResponse("tab", "How many spaces do you want a tab to be?")
+
+    setTimeout(() => {this.nextResponse()}, 250);
+    app = this;
+  }
+
+  addResponse(key, msg) {
+    const response = {
+      key: key,
+      msg: msg,
+      done: false,
+      active: false,
+    }
+    this.responses.push(response);
+  }
+
+  nextResponse() {
+    let hasNext = false;
+    for (let x in this.responses) {
+      const response = this.responses[x];
+      if (response.done) continue;
+
+      response.active = true;
+      this.addLine(response.msg)
+      hasNext = true
+      break;
+    }
+
+    if (!hasNext) {
+      ipcRenderer.send("addon-data", this.responseData)
+      this.addLine("Starting the creation of your addon...")
+    }
+  }
+
+  handleResponse(text) {
+    for (const x in this.responses) {
+      const response = this.responses[x];
+      if (response.completed || !response.active) continue;
+
+      this.responseData[response.key] = text;
+      response.done = true;
+      response.active = false;
+      this.nextResponse();
+      break;
+    }
+  }
+
+  addLine(text, color) {
+    const lines = this.state.lines;
+
+    lines.push({ text: text, color: color });
+    this.setState({
+      lines: lines,
+    })
+  }
+
+  handleKeyDown(event) {
+    if (event.key !== "Enter") return;
+    const input = event.target;
+    const text = input.value;
+
+    this.addLine(text, inputColor);
+    this.handleResponse(text);
+    input.value = "";
+  }
+
+  scrollToBottom() {
+    const elem = this.lastMessage;
+    if (!elem) return;
+
+    elem.scrollIntoView({ behavior: "smooth" });
+  }
+
+  render() {
+    const size = this.props.size;
+    const divStyle = {
+      width: size[0] - 26,
+      height: size[1] - 69,
+    }
+    return (
+      <div id="App" style={divStyle}>
+        <h1 id="title">GMod Addon Template Generator</h1>
+        
+        <div id="outputContainer">
+          {this.state.lines.map((line, i) =>
+            <Line key={i} text={line.text} color={line.color}/>
+          )}
+          <div ref={(el) => { this.lastMessage = el; this.scrollToBottom() }} />
+        </div>
+
+        <input id="input" onKeyDown={ event => this.handleKeyDown(event) }/>
+        {this.scrollToBottom()}
+      </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+class Line extends React.Component {
+  render() {
+    const pStyle = {
+      color: this.props.color || messageColor,
+    }
+    return (
+      <p className="line" style={pStyle}>{this.props.text}</p>
+    );
+  }
+}
+
+ReactDOM.render(<App size={size}/>, document.getElementById("root"));
+
+ipcRenderer.on("addon-done", (sender, data) => {
+  let str = data.success ? "Successfully created addon template..." : "Failed to create addon template..."
+  let col = !data.success && "red";
+  app.addLine(str, col)
+})
--- src/index.js
+++ src/index.js
@@ -1,18 +1,74 @@
-const { app, BrowserWindow } = require("electron");
-const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
-
-installExtension(REACT_DEVELOPER_TOOLS).then((name) => {
-  console.log(`Added Extension:  ${name}`);
-})
-  .catch((err) => {
-    console.log('An error occurred: ', err);
-  });
+const { app, BrowserWindow, ipcMain } = require("electron");
+const fs = require("fs");
+const path = require("path");
 
 app.on("ready", () => {
   const win = new BrowserWindow({
     width: 1000,
     height: 800,
+    resizable: false,
   });
   win.loadFile("./public/index.html");
   win.webContents.openDevTools();
 });
+
+function createDir(path) {
+  if (fs.existsSync(path)) return;
+  fs.mkdirSync(path);
+}
+
+function createAddon(sender, data) {
+  const appPath = path.dirname(app.getAppPath());
+  const dir = path.join(appPath, data.name)
+  const lua = path.join(dir, "lua");
+  const autorun = path.join(lua, "autorun");
+  const custom = path.join(lua, data.file_key)
+  const init = path.join(autorun, data.file_key + "_init.lua")
+  const config = path.join(custom, data.file_key + "_config.lua")
+  const sv = path.join(custom, data.file_key + "_sv.lua")
+  const cl = path.join(custom, data.file_key + "_cl.lua")
+  const sh = path.join(custom, data.file_key + "_sh.lua")
+  
+  const tonumber = Number(data.tab || 2)
+  const num = isNaN(tonumber) ? 2 : tonumber; 
+  let tab = " ";
+  tab = tab.repeat(num);
+  const double = tab.repeat(2);
+
+  let err = null;
+  try {
+    createDir(dir);
+    createDir(lua);
+    createDir(autorun);
+    createDir(custom);
+    fs.writeFileSync(config, `${data.global}.config = {} \n-----------------------------------------\n\n`)
+    fs.writeFileSync(init, 
+      `local function Initiate()
+${tab}${data.global} = ${data.global} or {}
+${tab}if SERVER then
+${double}include("${data.global}/${path.basename(config)}")
+${double}include("${data.global}/${path.basename(sh)}")
+${double}include("${data.global}/${path.basename(sv)}")
+${double}AddCSLuaFile("${data.global}/${path.basename(config)}")
+${double}AddCSLuaFile("${data.global}/${path.basename(sh)}")
+${double}AddCSLuaFile("${data.global}/${path.basename(sh)}")
+${tab}else
+${double}include("${data.global}/${path.basename(config)}")
+${double}include("${data.global}/${path.basename(sh)}")
+${double}include("${data.global}/${path.basename(sh)}")
+${tab}end
+end
+Initiate()`);
+    fs.writeFileSync(sv, "if true then end")
+    fs.writeFileSync(cl, "if true then end")
+    fs.writeFileSync(sh, "if true then end")
+  } catch (error) {
+    err = error;
+    console.error(error);
+  }
+  sender.send("addon-done", { success: !err })
+}
+
+ipcMain.on('addon-data', (event, data) => {
+  createAddon(event.sender, data);
+})
